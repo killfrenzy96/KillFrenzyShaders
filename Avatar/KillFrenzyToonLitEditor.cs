9825d8aaf81f3614441ff2a07140d0e7074f4eb3
killfrenzy96 <evan298@gmail.com>
2022-11-23 20:56:16 +1100

    bumped version number

--- Avatar/KillFrenzyToonLitEditor.cs
+++ Avatar/KillFrenzyToonLitEditor.cs
@@ -96,7 +96,7 @@ public class KillFrenzyToonLitEditor: ShaderGUI
 		if (featureEnabled.hsb) DrawHSB(materialEditor, ref featureShow.hsb);
 		DrawAdvanced(materialEditor, ref featureShow.advanced);
 
-		DrawLabel("KillFrenzy's Avatar Toon Lit Shaders v0.9.2-alpha3");
+		DrawLabel("KillFrenzy's Avatar Toon Lit Shaders v0.9.3");
 	}
 
 	private void DrawMain(MaterialEditor materialEditor, ref bool show) {
