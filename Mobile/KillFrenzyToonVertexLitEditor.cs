469162b7bb7550736befcdfc9b1e4c57451aaa17
killfrenzy96 <evan298@gmail.com>
2022-12-27 18:37:23 +1100

    updated version number

--- Mobile/KillFrenzyToonVertexLitEditor.cs
+++ Mobile/KillFrenzyToonVertexLitEditor.cs
@@ -70,7 +70,7 @@ public class KillFrenzyToonVertexLitEditor: ShaderGUI
 		if (featureEnabled.matCap) DrawMatCap(materialEditor, ref featureShow.matCap);
 		DrawAdvanced(materialEditor, ref featureShow.advanced);
 
-		DrawLabel("KillFrenzy's Mobile Avatar Toon VertexLit Shaders v0.9.3");
+		DrawLabel("KillFrenzy's Mobile Avatar Toon VertexLit Shaders v0.9.4");
 	}
 
 	private void DrawMain(MaterialEditor materialEditor, ref bool show) {
