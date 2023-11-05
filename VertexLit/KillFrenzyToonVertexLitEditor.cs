d3e94e15b875f7ee2a6e321dd74a534b23fac328
killfrenzy96 <evan298@gmail.com>
2023-11-05 19:02:28 +1100

    bump version number

--- VertexLit/KillFrenzyToonVertexLitEditor.cs
+++ VertexLit/KillFrenzyToonVertexLitEditor.cs
@@ -71,7 +71,7 @@ public class KillFrenzyToonVertexLitEditor: ShaderGUI
 		if (featureEnabled.matCap) DrawMatCap(materialEditor, ref featureShow.matCap);
 		DrawAdvanced(materialEditor, ref featureShow.advanced);
 
-		DrawLabel("KillFrenzy's Avatar Toon VertexLit Shaders v0.9.8b");
+		DrawLabel("KillFrenzy's Avatar Toon VertexLit Shaders v0.9.8c");
 	}
 
 	private void DrawMain(MaterialEditor materialEditor, ref bool show) {
