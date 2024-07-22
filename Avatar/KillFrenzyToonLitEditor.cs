6ccc9ca0005cfcceed664629b798e8e5e97f7e4a
killfrenzy96 <evan298@gmail.com>
2024-07-23 00:49:32 +1000

    bumped version number

--- Avatar/KillFrenzyToonLitEditor.cs
+++ Avatar/KillFrenzyToonLitEditor.cs
@@ -104,7 +104,7 @@ public class KillFrenzyToonLitEditor: ShaderGUI
 		if (featureEnabled.hsb) DrawHSB(materialEditor, ref featureShow.hsb);
 		DrawAdvanced(materialEditor, ref featureShow.advanced);
 
-		DrawLabel("KillFrenzy's Avatar Toon Lit Shaders v0.9.8h");
+		DrawLabel("KillFrenzy's Avatar Toon Lit Shaders v0.9.9");
 	}
 
 	private void DrawMain(MaterialEditor materialEditor, ref bool show) {
--- VertexLit/KillFrenzyToonVertexLitEditor.cs
+++ VertexLit/KillFrenzyToonVertexLitEditor.cs
@@ -71,7 +71,7 @@ public class KillFrenzyToonVertexLitEditor: ShaderGUI
 		if (featureEnabled.matCap) DrawMatCap(materialEditor, ref featureShow.matCap);
 		DrawAdvanced(materialEditor, ref featureShow.advanced);
 
-		DrawLabel("KillFrenzy's Avatar Toon VertexLit Shaders v0.9.8h");
+		DrawLabel("KillFrenzy's Avatar Toon VertexLit Shaders v0.9.9");
 	}
 
 	private void DrawMain(MaterialEditor materialEditor, ref bool show) {
